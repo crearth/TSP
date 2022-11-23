534ddb6e202883cff55f6d5c1bef0af24cb133f7
Arthur <arthur.cremelie@gmail.com>
2022-11-23 21:24:24 +0100

    init tabu search classes

--- src/tsp/TabuSearch.java
+++ src/tsp/TabuSearch.java
@@ -1,4 +1,25 @@
 package tsp;
 
-public class TabuSearch {
+import java.util.Queue;
+
+public class TabuSearch implements TabuSearchInterface{
+    /**
+     * Variable keeping the maximum amount of iterations.
+     */
+    private int maxIterations;
+    /**
+     * Variable keeping the graph.
+     */
+    private Graph graph;
+    private Queue<Integer> tabuList;
+
+    @Override
+    public void tabuSearch() {
+
+    }
+
+    @Override
+    public void twoOpt(int i, int j) {
+
+    }
 }
---
+++ src/tsp/TabuSearchInterface.java
@@ -0,0 +1,6 @@
+package tsp;
+
+public interface TabuSearchInterface {
+    void tabuSearch();
+    void twoOpt(int i, int j);
+}
